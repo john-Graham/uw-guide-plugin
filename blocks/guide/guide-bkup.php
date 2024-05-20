240613057c7a2734c8ec7d9bd329f30a78fe39fb
John Graham <jdgraham2@wisc.edu>
2024-05-20 17:00:38 -0500

    refactored Key, added shortcode and broke into more modules (recommit)

--- blocks/guide/guide-bkup.php
+++ blocks/guide/guide-bkup.php
@@ -14,42 +14,42 @@
 
 // Support custom "anchor" values.
 $anchor = '';
-if (!empty($block['anchor'])) {
+if (!empty($block['anchor'])) :
     $anchor = 'id="' . esc_attr($block['anchor']) . '" ';
-}
+endif;
 
 // Only allow and force align to be full
 $block['align'] == 'full';
 
 // WP block settings
 $classes = array('acf-guide', $block['id'], 'position-relative');
-if (!empty($block['className'])) {
+if (!empty($block['className'])) :
     $classes = array_merge($classes, explode(' ', $block['className']));
-}
-if (!empty($block['align'])) {
+endif;
+if (!empty($block['align'])) :
     $classes[] = 'align' . $block['align'];
-}
-if (empty($block['className']) || (isset($block['className']) && !preg_match('(m[tebsxy]{0,1}?-n?\d)', $block['className']))) {
+endif;
+if (empty($block['className']) || (isset($block['className']) && !preg_match('(m[tebsxy]{0,1}?-n?\d)', $block['className']))) :
     $classes[] = 'my-5';
-}
-if ((empty($block['className']) || (isset($block['className']) && !preg_match('(p[tebsxy]{0,1}?-n?\d)', $block['className']))) && (isset($block['align']) && $block['align'] == 'full')) {
+endif;
+if ((empty($block['className']) || (isset($block['className']) && !preg_match('(p[tebsxy]{0,1}?-n?\d)', $block['className']))) && (isset($block['align']) && $block['align'] == 'full')) :
     $classes[] = 'py-5';
-}
+endif;
 
 $container_classes = get_field('block_wrapper_class') ? explode(' ', get_field('block_wrapper_class')) : array();
-if ($block['align'] == 'full') {
+if ($block['align'] == 'full') :
     $container_classes[] = get_theme_mod('theme_container_type');
-}
+endif;
 
 $styles = array();
-if (!empty($block['gradient'])) {
+if (!empty($block['gradient'])) :
     $styles[] = 'background: var(--wp--preset--gradient--' . $block['gradient'] . ')';
-}
-if (!empty($block['style'])) {
-    if (!empty($block['style']['color']['gradient'])) {
+endif;
+if (!empty($block['style'])) :
+    if (!empty($block['style']['color']['gradient'])) :
         $styles[] = 'background: ' . $block['style']['color']['gradient'];
-    }
-}
+    endif;
+endif;
 
 // Block configuration settings
 $last_modified = get_field('last_modified');
@@ -58,7 +58,6 @@ $section = get_field('section');
 $find_replace = get_field('find_replace');
 $adjust_tags = get_field('adjust_tags');
 $graduate_section = get_field('graduate_section');
-$block_id = get_field('shortcode_id'); //calling it block_id when getting from the block, shortcode_id when getting from the CPT
 
 $original_unwrap_tags = get_field('remove_tags');
 $unwrap_tags = is_array($original_unwrap_tags) ? array_map(function ($item) {
@@ -71,7 +70,27 @@ $h_select = array(
     'select_title' => get_field('select_title')
 );
 
+// // Check if the shortcode_id is present; if not, create a new CPT
+// $shortcode_id = get_field('shortcode_id');
+// if (empty($shortcode_id)) {
+//     echo error_log('shortcode_id is empty, creating new CPT');
+//     // If no shortcode_id, generate one and create a new CPT
+//     $shortcode_id = wp_generate_uuid4();
 
+//     // Create the new CPT
+//     $content = ''; // If you need to fetch content for the new CPT, do it here
+//     $cpt_post_id = uwguide_create_cpt($url, $section, $last_modified, $content, $post_id, $shortcode_id); // Function to create CPT and return its post ID
+
+//     echo error_log('post_id of initial create is ' . $cpt_post_id);
+
+//     // Save the generated shortcode_id back to the block's field using the field key
+//     update_field('shortcode_id', $shortcode_id, $post_id);
+
+//     // Save the shortcode_id to the newly created CPT
+//     update_post_meta($cpt_post_id, 'shortcode_id', $shortcode_id);
+// } else {
+//     echo error_log('shortcode_id found: ' . $shortcode_id);
+// }
 ?>
 
 <?php
@@ -82,22 +101,21 @@ $rendor_in_admin_only = get_field('rendor_in_admin_only');
 // Check if we are in the admin preview or if the content should always be displayed
 if (!$rendor_in_admin_only || $is_preview) {
 
-
-    // Display admin-specific message at the top if in admin preview and content is admin-only
-    if ($is_preview && $rendor_in_admin_only) {
-        echo '<div style="background-color:#ccc">  <p><strong>Admin Only:</strong> This block is set to only display when editing.  This is likely in order to create a shortcode to use on this page.   This black may be placed anywhere on the page, suggestion is at the bottom of the page out of the way.   In order to have this display on the page use this guide with a shortcode, use: [uw-guide id="' . $block_id . '"]</p>';
-    }
+    // Additional style for admin preview
+    $admin_style = $is_preview ? 'background-color: #f7f7f7;' : '';
 
 ?>
-    <div <?php echo $anchor; ?> class="<?php echo esc_attr(implode(' ', $classes)); ?>" style="<?php echo $styles ? ' ' . esc_attr(implode(';', $styles)) : ''; ?>">
+    <div <?php echo $anchor; ?> class="<?php echo esc_attr(implode(' ', $classes)); ?>" style="<?php echo $admin_style;
+                                                                                                echo $styles ? ' ' . esc_attr(implode(';', $styles)) : ''; ?>">
         <?php if ($container_classes) : ?><div class="<?php echo esc_attr(implode(' ', $container_classes)); ?>"><?php endif; ?>
 
             <?php
             // Now run uwguide_entry using the shortcode_id
-            $cpt_post_id = uwguide_entry($url, $section, $find_replace, $adjust_tags, $graduate_section, $post_id, $unwrap_tags, $h_select, $block_id);
+            // $cpt_post_id = uwguide_entry($url, $section, $find_replace, $adjust_tags, $graduate_section, $post_id, $unwrap_tags, $h_select, $shortcode_id);
 
-            if ($is_preview && empty($cpt_post_id)) {
-                echo '<p>There is no content for this block.  Please check the settings and try again.</p>';
+            // Display admin-specific message at the top if in admin preview and content is admin-only
+            if ($is_preview && $rendor_in_admin_only) {
+                echo '<p><strong>Admin Only:</strong> To use this guide with a shortcode, use: [uw-guide id="' . $shortcode_id . '"]</p>';
             }
 
             ?>
@@ -105,9 +123,7 @@ if (!$rendor_in_admin_only || $is_preview) {
             <?php if ($container_classes) : ?></div><?php endif; ?>
     </div>
 <?php
-    if ($is_preview && $rendor_in_admin_only) {
-        echo '</div>';
-    }
 } // This closes the initial condition check
 
+// echo do_shortcode('[uw-guide id=”block_915f33af0798d5ce196d516e45c520fb”]');
 ?>
--- guide-content.php
+++ guide-content.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name:       CoE Guides
  * Description:       Wordpress plugin for CPT guide and api/xml call to guide.wisc.edu.
@@ -14,12 +15,14 @@
  */
 
 
- 
+
 // Define our handy constants.
-define( 'COE_GUIDES_VERSION', '0.1' );
-define( 'COE_GUIDES_PLUGIN_DIR', __DIR__ );
-define( 'COE_GUIDES_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-define( 'COE_GUIDES_PLUGIN_BLOCKS', COE_GUIDES_PLUGIN_DIR . '/blocks/' );
+define('COE_GUIDES_VERSION', '0.1');
+define('COE_GUIDES_PLUGIN_DIR', __DIR__);
+define('COE_GUIDES_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('COE_GUIDES_PLUGIN_BLOCKS', COE_GUIDES_PLUGIN_DIR . '/blocks/');
+
+define('WP_POST_REVISIONS', 3);
 
 // Set custom load & save JSON points for ACF sync.
 require 'includes/acf-json.php';
@@ -33,20 +36,44 @@ require 'includes/acf-restrict-access.php';
 require 'includes/template-tags.php';
 
 
-
-
 // functions used in admin and public
-require_once plugin_dir_path( __FILE__) . 'includes/core-functions.php';
+require_once plugin_dir_path(__FILE__) . 'includes/core-functions.php';
+
+// functions used for cleaning up the content
+require_once plugin_dir_path(__FILE__) . 'includes/node-functions.php';
 
 
 // default plugin options
-function guide_content_options_default() {
+function guide_content_options_default()
+{
 
 	return array(
 		'custom_title'   => 'default variables if needed',
 
 	);
-
 }
 
+// setting a default value for shortcode_id when blocks are created
+function generate_alphanumeric_id($length = 8) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[random_int(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
 
+add_filter('acf/load_value/name=shortcode_id', 'set_default_shortcode_id', 10, 3);
+
+function set_default_shortcode_id($value, $post_id, $field) {
+    if (empty($value)) {
+        $value = generate_alphanumeric_id();
+    }
+    return $value;
+}
+
+// creating a hash to check if fields have changed
+function generate_fields_hash($fields) {
+    return md5(serialize($fields));
+}
